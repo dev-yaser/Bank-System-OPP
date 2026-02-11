b4b85226a7c3445ee6d193526bbbae4570c67cee
dev-yaser <yaser.codes@gmail.com>
2026-02-11 09:13:10 +0200

    add manage users

--- Bank-project(OPP)/clsMainScreen.h
+++ Bank-project(OPP)/clsMainScreen.h
@@ -8,7 +8,7 @@
 #include "clsUpdateClientScreen.h"
 #include "clsFindClientScreen.h"
 #include "clsTransactionsScreen.h"
-
+#include "clsManageUsers.h"
 using namespace std;
 
 class clsMainScreen :protected clsScreen
@@ -84,8 +84,8 @@ private:
 
     static void _ShowManageUsersMenue()
     {
-        cout << "\nUsers Menue Will be here...\n";
-
+        // cout << "\nUsers Menue Will be here...\n";
+        clsManageUsersScreen::ShowManageUsersMenue();
     }
 
     static void _ShowEndScreen()
