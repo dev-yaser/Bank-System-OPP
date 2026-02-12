2ed42cd220d5ac8f49f82ab3a3b69d5132375cd7
dev-yaser <yaser.codes@gmail.com>
2026-02-12 10:14:30 +0200

    add user update

--- Bank-project(OPP)/clsManageUsers.h
+++ Bank-project(OPP)/clsManageUsers.h
@@ -6,6 +6,7 @@
 #include "clsListUsersScreen.h"
 #include "clsAddNewUserScreen.h"
 #include "clsDeleteUserScreen.h"
+#include "clsUpdateUserScreen.h"
 
 using namespace std;
 
@@ -55,7 +56,9 @@ private:
 
     static void _ShowUpdateUserScreen()
     {
-        cout << "\nUpdate User Screen Will Be Here.\n";
+        
+        /*cout << "\nUpdate User Screen Will Be Here.\n";*/
+		clsUpdateUserScreen::ShowUpdateUserScreen();
 
     }
 
