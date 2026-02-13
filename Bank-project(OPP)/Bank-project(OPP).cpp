d403940b0c8008a1870c3df37040a973ad4b7f8c
dev-yaser <yaser.codes@gmail.com>
2026-02-13 20:59:36 +0200

    add login section

--- Bank-project(OPP)/Bank-project(OPP).cpp
+++ Bank-project(OPP)/Bank-project(OPP).cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
-#include "clsMainScreen.h"	
+#include "clsLoginScreen.h"
 int main()
 {
-	clsMainScreen::ShowMainMenue();
+	clsLoginScreen::ShowLoginScreen();
+	system("pause>0");
 	return 0;
 }
+
