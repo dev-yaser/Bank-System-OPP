a946a95a8f151ff48fbbad300d37221eeacaf5ae
dev-yaser <yaser.codes@gmail.com>
2026-02-15 10:58:33 +0200

    add date

--- Bank-project(OPP)/clsScreen.h
+++ Bank-project(OPP)/clsScreen.h
@@ -2,13 +2,14 @@
 #include <iostream>
 #include "clsUser.h"
 #include "Global.h"
+#include "clsDate.h"
 
 using namespace std;
 
 class clsScreen
 {
-protected :
-    static void _DrawScreenHeader(string Title,string SubTitle ="")
+protected:
+    static void _DrawScreenHeader(string Title, string SubTitle = "")
     {
         cout << "\t\t\t\t\t______________________________________";
         cout << "\n\n\t\t\t\t\t  " << Title;
@@ -16,25 +17,28 @@ protected :
         {
             cout << "\n\t\t\t\t\t  " << SubTitle;
         }
-        cout << "\n\t\t\t\t\t______________________________________\n\n";
+        cout << "\n\t\t\t\t\t______________________________________\n";
+        cout << "\n\t\t\t\t\tUser: " << CurrentUser.UserName << "\n";
+        cout << "\t\t\t\t\tDate: " << clsDate::DateToString(clsDate())
+            << "\n\n";
+
     }
 
     static bool CheckAccessRights(clsUser::enPermissions Permission)
     {
-     
-            if (!CurrentUser.CheckAccessPermission(Permission))
-            {
-                cout << "\t\t\t\t\t______________________________________";
-                cout << "\n\n\t\t\t\t\t  Access Denied! Contact your Admin.";   
-                cout << "\n\t\t\t\t\t______________________________________\n\n";
-                return false;
-            }
-            else
-            {
-                return true;
-            }
+
+        if (!CurrentUser.CheckAccessPermission(Permission))
+        {
+            cout << "\t\t\t\t\t______________________________________";
+            cout << "\n\n\t\t\t\t\t  Access Denied! Contact your Admin.";
+            cout << "\n\t\t\t\t\t______________________________________\n\n";
+            return false;
+        }
+        else
+        {
+            return true;
+        }
 
     }
 
 };
-
